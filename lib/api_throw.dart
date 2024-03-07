a98743983bb5cc049740fea15303fb51e5971e3b
hieu nguyen <hieu.nguyen2@bnksolution.com>
2024-03-07 09:49:37 +0700

    first commit

---
+++ lib/api_throw.dart
@@ -0,0 +1,7 @@
+class ApiThrow {
+  const ApiThrow(this.code, this.name, this.raw);
+
+  final int code;
+  final String name;
+  final String raw;
+}
---
+++ lib/base_list_data.dart
@@ -0,0 +1,80 @@
+library base_list_data;
+
+import 'package:base_list_data/api_throw.dart';
+
+class BaseListData<T, Y> {
+  final Function(ApiThrow e) onError;
+  final void Function(void Function()) onRerender;
+  final Future<List<T>> Function(int page, int limit) onLoadMore;
+  final Y key;
+  final List<T> initList;
+
+  BaseListData({
+    required this.key,
+    this.limit = 10,
+    this.initList = const [],
+    required this.onRerender,
+    required this.onLoadMore,
+    required this.onError,
+  }) {
+    list.addAll(initList);
+  }
+
+  final List<T> list = [];
+  bool max = false;
+  int limit;
+  bool _loading = false;
+  int _page = 0;
+  int _count = 0;
+
+  bool get loadingMore => _page > 0 && _loading;
+
+  bool get loading => _page == 0 && _loading;
+
+  Future<void> getList({reset = false}) async {
+    try {
+      if (!reset && (_loading || max)) return;
+
+      onRerender(() {
+        _loading = true;
+
+        if (reset) {
+          list.clear();
+          list.addAll(initList);
+          _page = 0;
+          max = false;
+        }
+      });
+
+      ++_count;
+      final currentCount = _count + 0;
+
+      final r = await onLoadMore(_page + 1, limit);
+
+      if (currentCount != _count) return;
+
+      onRerender(() {
+        list.addAll(r);
+        _page += 1;
+        _loading = false;
+
+        if (r.length < limit) max = true;
+      });
+    } on ApiThrow catch (e) {
+      onRerender(() {
+        _loading = false;
+      });
+
+      onError(e);
+    }
+  }
+
+  void updateItem(int i, T newData) {
+    if (i == -1) return;
+
+    onRerender(() {
+      list.removeAt(i);
+      list.insert(i, newData);
+    });
+  }
+}
